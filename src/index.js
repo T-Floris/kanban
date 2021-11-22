363e35da1779646db3029bc8419dd6a41386c393
Hao <54834780+HaoYeChen@users.noreply.github.com>
2021-11-22 14:28:47 +0100

    removed unused code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Board from "./components/Kanban/board";
-import { Provider } from "react-redux";
-import store from "./redux/store";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
+
       <App />
-      {/* <Board />
-    </Provider> */}
+
   </React.StrictMode>,
   document.getElementById("root")
 );
