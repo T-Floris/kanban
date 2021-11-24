2d10f4fb8c12c607d2c4d3c9799148f7b24b3780
Hao <54834780+HaoYeChen@users.noreply.github.com>
2021-11-24 13:01:59 +0100

    removed navbar from sign in page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,18 +1,10 @@
 import React, { useState } from "react";
-import Navbar from "../components/Navbar";
-
-import Sidebar from "../components/Sidebar";
 
 const SigninPage = () => {
-  const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = () => {
-    setIsOpen(!isOpen);
-  };
   return (
     <div>
-      <Sidebar isOpen={isOpen} toggle={toggle} />
-      <Navbar toggle={toggle} />
+      <p>sign in</p>
     
     </div>
   );
