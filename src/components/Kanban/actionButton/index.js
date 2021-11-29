628935c9ef4715f90c06088016bcb0052e7bab25
Hao <54834780+HaoYeChen@users.noreply.github.com>
2021-11-29 10:34:38 +0100

    fixed typo

--- src/components/Kanban/actionButton/index.js
+++ src/components/Kanban/actionButton/index.js
@@ -59,7 +59,7 @@ class TrelloActionButton extends React.Component {
   renderAddButton = () => {
     const { list } = this.props;
 
-    const buttonText = list ? "add another list" : "Add another card";
+    const buttonText = list ? "Add another list" : "Add another card";
     const buttonTextOpacity = list ? 1 : 0.5;
     const buttonTextColor = list ? "white" : "inherit";
     const buttonTextBackground = list ? "rgba(0,0,0,.15)" : "inherit";
